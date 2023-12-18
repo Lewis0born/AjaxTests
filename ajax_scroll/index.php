d46778e3c74388120443022d8de2156af603c6a8
Lewis0born <lewis.oborn@gmail.com>
2023-12-18 12:38:57 +1030

    Add ability to load more pages to bottom of page

--- ajax_scroll/index.php
+++ ajax_scroll/index.php
@@ -19,18 +19,6 @@
   </head>
   <body>
     <div id="blog-posts">
-      <div id="blog-post-101" class="blog-post">
-        <h3>Blog Post 101</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
-      <div id="blog-post-102" class="blog-post">
-        <h3>Blog Post 102</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
-      <div id="blog-post-103" class="blog-post">
-        <h3>Blog Post 103</h3>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed scelerisque nunc malesuada mauris fermentum commodo. Integer non pellentesque augue, vitae pellentesque tortor. Ut gravida ullamcorper dolor, ac fringilla mauris interdum id. Nulla porta egestas nisi, et eleifend nisl tincidunt suscipit. Suspendisse massa ex, fringilla quis orci a, rhoncus porta nulla. Aliquam diam velit, bibendum sit amet suscipit eget, mollis in purus. Sed mattis ultricies scelerisque. Integer ligula magna, feugiat non purus eget, pharetra volutpat orci. Duis gravida neque erat, nec venenatis dui dictum vel. Maecenas molestie tortor nec justo porttitor, in sagittis libero consequat. Maecenas finibus porttitor nisl vitae tincidunt.</p>
-      </div>
     </div>
 
     <div id="spinner">
@@ -38,7 +26,7 @@
     </div>
 
     <div id="load-more-container">
-      <button id="load-more">Load more</button>
+      <button id="load-more" data-page="0">Load more</button>
     </div>
 
     <script>
@@ -64,13 +52,38 @@
         load_more.style.display = 'none';
       }
 
+      // put new html into a temp div
+      // this causes browser to parse it as elements.
+      function appendToDiv(div, newHTML){
+        let temp = document.createElement('div');
+        temp.innerHTML = newHTML;
+
+        // find and work with those elements
+        // use firstElementChild because of how DOM treats whitespace
+        let class_name = temp.firstElementChild.className;
+        let items = temp.getElementsByClassName(class_name);
+
+        let len = items.length;
+        for(i=0; i<len; i++){
+          div.appendChild(items[0]);
+        }
+      }
+
+      function setCurrentPage(page){
+        console.log('Incrementing page to: ' + page);
+        load_more.setAttribute('data-page', page);
+      }
+
       function loadMore() {
 
         showSpinner();
         hideLoadMore();
 
+        let page = parseInt(load_more.getAttribute('data-page'));
+        let next_page = page + 1;
+
         var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'blog_posts.php?page=1', true);
+        xhr.open('GET', 'blog_posts.php?page=' + next_page, true);
         xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
         xhr.onreadystatechange = function () {
           if(xhr.readyState == 4 && xhr.status == 200) {
@@ -78,7 +91,9 @@
             console.log('Result: ' + result);
 
             hideSpinner();
+            setCurrentPage(next_page);
             // append results to end of blog posts
+            appendToDiv(container,result);
             showLoadMore();
 
           }
@@ -88,8 +103,10 @@
 
       load_more.addEventListener("click", loadMore);
 
+      
+
       // Load even the first page with Ajax
-      //loadMore();
+      loadMore();
     </script>
 
   </body>
